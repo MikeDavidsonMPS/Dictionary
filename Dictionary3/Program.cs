2d230260b7be8addd3bc52934435c525c162f04a
michael Davidson <meridianproservice@outlook.com>
2021-04-27 19:16:01 -0400

    Add project files.

---
+++ Dictionary3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        public static void Main()
+        {
+            Country country1 = new Country() { Code = "AUS", Name = "Australia", Capital = "Canberra" };
+            Country country2 = new Country() { Code = "IND", Name = "INDIA", Capital = "New Delhi" };
+        }
+    }
---
+++ Dictionary3/triminal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary3
+{
+    public class triminal
+    {
+        public class Country
+        {
+            public string Name { get; set; }
+            public string Code { get; set; }
+            public string Capital { get; set; }
+        }
+
+        public static void Main()
+        {
+            Country country1 = new Country() { Code = "AUS", Name = "Australia", Capital = "Canberra" };
+            Country country2 = new Country() { Code = "IND", Name = "INDIA", Capital = "New Delhi" };
+            Country country3 = new Country() { Code = "USA", Name = "UNITED STATES", Capital = "Washington DC" };
+            Country country4 = new Country() { Code = "GBR", Name = "UNITED KINGDOM", Capital = "London" };
+            Country country5 = new Country() { Code = "CAN", Name = "CANADA", Capital = "Ottawa" };
+
+            Dictionary<string, Country> dictionaryCountries = new Dictionary<string, Country>;
+            dictionaryCountries.Add(country1.Code, country1);
+            dictionaryCountries.Add(country2.Code, country2);
+            dictionaryCountries.Add(country3.Code, country3);
+            dictionaryCountries.Add(country4.Code, country4);
+            dictionaryCountries.Add(country5.Code, country5);
+
+            string strUserChoice = string.Empty;
+            do
+            {
+                Console.WriteLine("Please enter Country code");
+                string strCountryCode = Console.ReadLine().ToUpper();
+
+                Country resultCountry = dictionaryCountries.ContainsKey(strCountryCode) ? dictionaryCountries[strCountryCode] : null;
+
+                if (resultCountry == null)
+                {
+                    Console.WriteLine("Country code not valid");
+                }
+                else
+                {
+                    Console.WriteLine("Name = {0}, Capital {1}", resultCountry.Name, resultCountry.Capital);
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    strUserChoice = Console.ReadLine().ToUpper();
+                } while (strUserChoice != "N" && strUserChoice != "Y");
+            }
+            while (strUserChoice == "Y");
+        }
+    }
+}
